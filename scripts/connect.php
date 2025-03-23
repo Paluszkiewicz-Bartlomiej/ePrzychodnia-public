d1183eda6cac13811522b9ac905b9861b643c491
Paluszkiewicz-Bartlomiej <71929413+Paluszkiewicz-Bartlomiej@users.noreply.github.com>
2025-03-23 18:14:49 +0100

    moved db credentials to env variables

--- scripts/connect.php
+++ scripts/connect.php
@@ -1,5 +1,8 @@
 <?php
-    $conn = new mysqli('database', 'admin', 'P@ssw0rd', 'clinic');
+    $url = getenv('MYSQL_HOST');
+    $user = getenv('MYSQL_USERNAME');
+    $pass = getenv('MYSQL_PASSWORD');
+    $conn = new mysqli($url, $user, $pass, 'clinic');
     //$today = date("Y-m-d");
     $today = date('Y-m-d H:i:s');
 ?>
